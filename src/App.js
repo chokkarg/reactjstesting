c6c18a680c9f835970d7bf4a34c8048492a425d0
chokkar.g <chokkar.g@dish.com>
2023-01-17 14:34:07 +0530

    Sending props from parent

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HeaderComp from './component/Headercomp';
+
+function App() {
+
+  return (    
+    <div className="App">
+      <p> This is app component </p>
+      <HeaderComp dishcolor="green"></HeaderComp>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Headercomp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeaderComp({dishcolor}) {
+  return (
+    <div>
+     <h1 style={{color:dishcolor}}>Text</h1>
+     </div>
+  )
+}
