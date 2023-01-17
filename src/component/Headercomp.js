fc3b7b5ad9fc49c2e32dafd9f3b6bd512b1503b6
chokkar.g <chokkar.g@dish.com>
2023-01-17 14:53:46 +0530

    Text change

--- src/component/Headercomp.js
+++ src/component/Headercomp.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HeaderComp({dishcolor}) {
   return (
     <div>
-     <h1 style={{color:dishcolor}}>Text</h1>
+     <h1 style={{color:dishcolor}}>This green color came from app component</h1>
      </div>
   )
 }
